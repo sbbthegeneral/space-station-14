f0e53a963fc8fbc1d93469ba2918bcbacb416d7d
Steven <sbbthegeneral@gmail.com>
2025-04-22 21:52:06 +1000

    Added shared voice chat component and server voice chat system

---
+++ Content.Server/VoiceChat/VoiceChatSystem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robust.Shared.Network;
+
+namespace Content.Server.VoiceChat
+{
+    internal class VoiceChatSystem
+    {
+        NetManager.
+    }
+}
