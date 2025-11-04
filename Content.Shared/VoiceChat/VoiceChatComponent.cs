f1dc964b07ef5f75cb78281902183081d22d987e
Steven <sbbthegeneral@gmail.com>
2025-11-05 02:20:09 +1100

    Created files for server side and client side voice chat system and created a voice chat component in shared

---
+++ Content.Shared/VoiceChat/VoiceChatComponent.cs
@@ -0,0 +1,13 @@
+using Robust.Shared.GameStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Shared.VoiceChat;
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+internal class VoiceChatComponent : Component
+{
+}
