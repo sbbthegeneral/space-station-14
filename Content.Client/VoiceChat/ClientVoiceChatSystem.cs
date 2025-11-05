1f00bc4c41d0361d10b80ee1bdd7f1fbc9693794
Steven <sbbthegeneral@gmail.com>
2025-11-05 20:28:43 +1100

    Subscribed to local events relating to a player joining the lobby. Added OpenTK.Audio.OpenAL package to server, client, and shared

--- Content.Client/VoiceChat/ClientVoiceChatSystem.cs
+++ Content.Client/VoiceChat/ClientVoiceChatSystem.cs
@@ -1,10 +1,24 @@
+using Content.Shared.GameTicking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
 
 namespace Content.Client.VoiceChat;
 public sealed class ClientVoiceChatSystem : EntitySystem
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<TickerJoinLobbyEvent>(OnJoinLobby);
+    }
+
+    public void OnJoinLobby(TickerJoinLobbyEvent args)
+    {
+
+    } 
+
 }
--- Content.Server/VoiceChat/ServerVoiceChatSystem.cs
+++ Content.Server/VoiceChat/ServerVoiceChatSystem.cs
@@ -1,10 +1,24 @@
+using Content.Server.GameTicking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
 
 namespace Content.Server.VoiceChat;
 public sealed class ServerVoiceChatSystem : EntitySystem
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PlayerJoinedLobbyEvent>(OnJoinLobby);
+    }
+
+    public void OnJoinLobby(PlayerJoinedLobbyEvent args)
+    {
+        
+    }
+
 }
